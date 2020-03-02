7ee0941b4fd1faa84b914eccc8ffdd40703a6f44
Tyler Richardson <tyler@quotedcar.com>
2020-03-02 10:48:01 -0800

    Removed http to https npm package, handling with Firebase instead

--- functions/index.js
+++ functions/index.js
@@ -1,32 +1,47 @@
-// Requirements
+/////////////////////////////////////////////////////////////////////////////////////////
+// index.js Requirements
+/////////////////////////////////////////////////////////////////////////////////////////
+
 const functions = require('firebase-functions');
 const express = require('express');
 const engines = require('consolidate');
 const hbs = require('hbs');
 const path = require('path');
-var redirectToHTTPS = require('express-http-to-https').redirectToHTTPS
+
+/////////////////////////////////////////////////////////////////////////////////////////
+// Express
+/////////////////////////////////////////////////////////////////////////////////////////
 
 // Create Express App
 const app = express();
+
+/////////////////////////////////////////////////////////////////////////////////////////
+// Template Engine
+/////////////////////////////////////////////////////////////////////////////////////////
 
 // Create Templating Engine
 app.engine('hbs', engines.handlebars);
 app.set('views', './views');
 app.set('view engine', 'hbs');
 
-// Redirect Attempted HTTP Connections to HTTPS
-// app.use(redirectToHTTPS([/localhost:(\d{4})/], [/\/insecure/], 301));
+/////////////////////////////////////////////////////////////////////////////////////////
+// Static Paths
+/////////////////////////////////////////////////////////////////////////////////////////
 
 // Create Paths to Link Plugins From Front End to Node Modules Directory
 app.use('/javascripts/plugins', express.static(path.join('node_modules', 'lazysizes')));
 
-// Create Routes
-app.get('/', function(req, res, next) {
-	// Cache Policy
-	res.set('Cache-Control', 'public, max-age=86400, s-maxage=86400');
-	// Render Response into the .hbs template
-	res.render('index', {});
-});
+/////////////////////////////////////////////////////////////////////////////////////////
+// Routes
+/////////////////////////////////////////////////////////////////////////////////////////
+
+	// Home Route
+	app.get('/', function(req, res, next) {
+		// Cache Policy
+		res.set('Cache-Control', 'public, max-age=86400, s-maxage=86400');
+		// Render Response into the .hbs template
+		res.render('index', {});
+	});
 
 // Deploy Express App As A Firebase Cloud Function
 exports.app = functions.https.onRequest(app);
