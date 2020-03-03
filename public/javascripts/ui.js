0c6c1827c6cfec30f61566885384d965dbb21cdb
Tyler Richardson <tyler@quotedcar.com>
2020-03-02 17:17:27 -0800

    Case Study Overview View

--- public/javascripts/ui.js
+++ public/javascripts/ui.js
@@ -1,4 +1,37 @@
 $(document).ready(function() {
+	////////////////////////////////////////////////
 	// Logotype Kerning
-	$(".hero-title").lettering();
+	function logotype() {
+		$(".hero-title").lettering();
+	}
+	logotype();
+	////////////////////////////////////////////////
+	// Navigation
+	$(document).on('click', '.nav-tab', function() {
+		$('.nav-content').toggleClass('nav-show');
+		$('.nav-tab').toggleClass('nav-close');
+	});
+	////////////////////////////////////////////////
+	// Scrolling Content Controls
+	function contentControls() {
+
+		$(document).scroll(function() {
+
+			var vH = $(window).height() - 100;
+			var scrollPosition = $(document).scrollTop();
+
+			console.log('VH is: ' + vH);
+			console.log('S is: ' + scrollPosition);
+
+			if(scrollPosition >= vH) {
+				$('.content-controls').removeClass('cc-hide');
+				$('.content-controls').fadeIn();
+			} else {
+				$('.content-controls').fadeOut();
+			}
+
+		});
+	}
+	contentControls();
+	////////////////////////////////////////////////
 });
